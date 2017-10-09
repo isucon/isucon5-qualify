ee1235c44b084e08e091e580cda487b9e9d568be
SAKATA Sinji <sinji@sakasin.net>
2017-10-09 20:32:30 +0900

    Use pprof

--- webapp/go/app.go
+++ webapp/go/app.go
@@ -10,6 +10,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"path"
 	"runtime"
@@ -780,6 +781,11 @@ func main() {
 	r.HandleFunc("/initialize", myHandler(GetInitialize))
 	r.HandleFunc("/", myHandler(GetIndex))
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../static")))
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
